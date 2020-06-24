df646f839236efc86a54b771767cb0d787b30ec2
takuyakawase <takuya86takuya@gmail.com>
2020-06-25 00:21:59 +0900

    view create

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * ログインフォーム表示アクション
+     * */
+    public function signin() {
+        return view('user.signin');
+    }
+}
