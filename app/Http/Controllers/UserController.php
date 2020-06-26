83e578062e836b900cafb84605da1697422c8b22
takuyakawase <takuya86takuya@gmail.com>
2020-06-27 00:02:38 +0900

    create実装

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\UserRequest;
 
 use Illuminate\Support\Facades\Auth;
 
+use App\User;
+
+use Hash;
+
 class UserController extends Controller
 {
     /**
@@ -40,5 +44,36 @@ class UserController extends Controller
     {
       Auth::logout();
       return redirect()->route('user.signin');
+    }
+
+    /**
+     * ユーザ登録ページ表示アクション
+     */
+    public function create()
+    {
+      return view('user.create');
+    }
+
+    /**
+     * ユーザ登録処理アクション
+     */
+    public function store(Request $request)
+    {
+      $user     = new User;
+      $name     = $request -> input('name');
+      $email    = $request -> input('email');
+      $password = $request -> input('password');
+      $params   = [
+        'name'     => $name,
+        'email'    => $email,
+        'password' => Hash::make($password),
+      ];
+      if (!$user -> fill($params) -> save()) {
+        return redirect() -> route('user.create') -> with('error_message', 'User registration failed');
+      }
+      if (!Auth::attempt(['email' => $email, 'password' => $password])) {
+        return redirect() -> route('user.signin') -> with('error_message', 'I failed to login');
+      }
+      return redirect() -> route('micropost.index');
     }
 }
