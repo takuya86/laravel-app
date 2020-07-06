6dcb2af68d3f8e7d913fa42c81a6b39fded31b3c
takuyakawase <takuya86takuya@gmail.com>
2020-07-07 00:44:46 +0900

    css修正中

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-<div class="card">
+<div class="card home-mein">
   <img class="card-img-top" src="/images/travel.png" alt="Card image cap">
   <div class="card-body prefs-choice">
-    <h4 class="card-title">さぁ、集まろう！</h4>
+    <h3 class="card-title home-title">さぁ、集まろう！</h3>
       <select type="text" class="form-control" name="area">
         @foreach(config('pref') as $key => $score)
             <option value="{{ $score }}">{{ $score }}</option>
